2c8774294ed3df1e83fe452bec7f6a9bf371e553
Nkechi Nnaji <nkechi_nnaji@gallup.com>
2025-09-06 03:12:53 -0500

    add script tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import {Script} from 'next'
 
 export const metadata = {
     title: {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <Script strategy="afterInteractive" src="/bannerscript.js" />
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import { Markdown } from 'components/markdown';
 import { RandomQuote } from 'components/random-quote';
 import { getNetlifyContext } from 'utils';
 
-import {Script} from 'next/script';
+import {Script} from 'next'
 
 const contextExplainer = `
 The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
@@ -29,10 +29,6 @@ export default function Page() {
     return (
         
         <div className="flex flex-col gap-12 sm:gap-16">
-            <Script>
-!function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AF||function(){(t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))},t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],o.async=1,o.src="https://websdk.appsflyer.com?"+(c.length>0?"st="+c.split(",").sort().join(",")+"&":"")+(i.length>0?"af_id="+i:""),p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "88249c60-8cca-4464-aca9-a63f5e3bd517"}});
-AF('banners', 'showBanner')
-</Script>
             <section>
                 <ContextAlert className="mb-6" />
                 <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
